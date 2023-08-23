89c1dbd4145fd9439d7f8f6c332bbf2f44136c42
Mike Allman <michaela@dunnhumby.com>
2023-08-23 09:35:11 +0100

    basic config for nvim-tree

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,12 +1,30 @@
+local function my_on_attach(bufnr)
+
+    local api = require "nvim-tree.api"
+
+    local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+
+    -- default mappings
+    api.config.mappings.default_on_attach(bufnr)
+
+    -- custom mappings
+    vim.keymap.set('n', '<C-t>', api.tree.toggle)
+    vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+end
+
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    width = 40,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+    sort_by = "case_sensitive",
+    view = {
+        width = 40,
+    },
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = true,
+    },
+    on_attach = my_on_attach,
 })
+
